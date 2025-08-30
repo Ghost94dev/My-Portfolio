faae8def69b76c27b09b4745617c1b594a8d9c38
Devalor <madozin.dev@gmail.com>
2025-08-30 22:37:01 +0000

    app name changes

--- portfolio/myportfolio/migrations/0002_alter_contact_number.py
+++ portfolio/myportfolio/migrations/0002_alter_contact_number.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('Myportfolio', '0001_initial'),
+        ('myportfolio', '0001_initial'),
     ]
 
     operations = [
