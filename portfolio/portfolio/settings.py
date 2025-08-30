8e7aacc38e366699d516736c411114a49bbfe117
Devalor <madozin.dev@gmail.com>
2025-08-30 00:01:49 +0000

    new-chang

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':  [BASE_DIR / 'Myportfolio/main_templates'],
+        'DIRS':  [BASE_DIR / 'Myportfolio'/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = BASE_DIR / "Myportfolio" / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
