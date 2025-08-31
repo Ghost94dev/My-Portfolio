bfaf317a52e0f9d55667d0f22f1fd71f51524384
Bengba Madozin Evlin Dev <51913734+Ghost94dev@users.noreply.github.com>
2025-08-31 03:48:34 +0000

    Update settings.py
    sett

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -68,6 +70,12 @@ TEMPLATES = [
         },
     },
 ]
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
+
+application = get_wsgi_application()
 
 WSGI_APPLICATION = 'portfolio.wsgi.application'
 
